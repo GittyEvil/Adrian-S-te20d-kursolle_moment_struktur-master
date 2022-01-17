eda7140a27fc56cd2c9c1a948d0edb2868e706c9
Your Name <you@example.com>
2022-01-17 16:58:47 +0100

    oåsdk

---
+++ moment04/inlamning04a.py
@@ -0,0 +1,17 @@
+sida1=int(input('välj ett första tal upp till tio.'))
+
+sida2=int(input('Välj ett andra tal upp till tio.'))
+
+area=sida1*sida2
+
+
+
+if sida1 == sida2:
+    print('båda sidorna är lika långa, därför är det en kvadrat.')
+
+print("Höjden | Volymen")
+print("----------------")
+for x in range(1,11):
+    print(x, "|")
+    print(area*x)
+
