6e2937de0cc7d66b9866f29f69f65f9ec868da58
Your Name <you@example.com>
2022-01-20 10:45:22 +0100

    ads

--- moment04/inlamning04c.py
+++ moment04/inlamning04c.py
@@ -1,11 +1,21 @@
 def kvadratspel():
-    sida1=int(input('välj ett första tal upp till tio.'))
+    sida1= input('välj ett första tal upp till tio.')
 
-    sida2=int(input('Välj ett andra tal upp till tio.'))
+    sida2= input('Välj ett andra tal upp till tio.')
 
-    area=sida1*sida2
+    if sida1.isnumeric() == False or sida1.isnumeric() == False:
+        print("det är ingen siffra, försök igen.")
+        kvadratspel()
+    sida1 = int(sida1)
+    sida2 = int(sida2)
 
+    if sida1 < 0:
+        sida1 == 1
 
+    if sida1 > 10:
+        sida1 == 10
+
+    area = sida1 * sida2
 
     if sida1 == sida2:
         print('båda sidorna är lika långa, därför är det en kvadrat.')
@@ -13,17 +23,16 @@ def kvadratspel():
     print("Höjden | Volymen")
     print("----------------")
     for x in range(1,11):
-        print("   {}    | {} ".format(x, x*area))
-    
+        print("  {}    |   {} ".format(x, area))
+
     köraigen=input("Vill du göra en beräkning till (ja/nej)?")
-    
+
     if köraigen == "ja":
         kvadratspel()
 
-    if köraigen == "nej": 
+    if köraigen == "nej":
         print("kod avslutad")
 
-    
+
 
 kvadratspel()
-#fixa så att den faktiskt avbtryts
