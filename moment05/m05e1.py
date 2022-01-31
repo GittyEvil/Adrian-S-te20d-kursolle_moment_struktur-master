63b070a8e94e7abca658bd6c65d272eb035d1cf9
Your Name <you@example.com>
2022-01-31 10:51:14 +0100

    assd

---
+++ moment05/m05e1.py
@@ -0,0 +1,7 @@
+def Adrian():
+    print("Jag heter Adrian Stude")
+    print("går i Te20D")
+    print("Går på NTI Kronhus")
+
+
+Adrian()
---
+++ moment05/m05e2.py
@@ -0,0 +1,25 @@
+
+def area():
+    global radie
+    arean = radie * 2 * 3
+    print(arean)
+
+def omkrets():
+    global radie
+
+    diameter = radie * 2
+
+    omkretsen = 3 * diameter
+
+    print(omkretsen)
+
+
+
+
+
+
+
+radie = input("välj ett tal som blir radie:")
+
+area()
+omkrets()
